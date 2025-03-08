e517318249400597fbcd61f1f065287ca59b5627
darpan neve <darpanneve3@gmail.com>
2025-03-08 09:25:11 +0530

    localhost removed

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "https://hiresphere-eita.onrender.com",
-        target: "http://localhost:8000",
+        target: "https://hiresphere-eita.onrender.com",
+        // target: "http://localhost:8000",
         changeOrigin: true,
         secure: true,
         headers: {
